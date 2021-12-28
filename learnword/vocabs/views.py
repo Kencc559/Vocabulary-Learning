2dcab06904bf101b264553f4b99aa7481587d2ca
ken_cc <ken_cc@gmail.com>
2021-12-28 17:10:51 +0800

    add login check in review_list and learned page

--- learnword/vocabs/views.py
+++ learnword/vocabs/views.py
@@ -1,16 +1,30 @@
 #file: vocabs/views.py
 
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+
+def check_login(fn):
+    def wrap(request, *args, **kwargs):
+        if not hasattr(request, 'session'):
+            return HttpResponseRedirect('/t1/user/login')
+        if 'user' not in request.session:
+            return HttpResponseRedirect('/t1/user/login')
+        return fn(request, *args, **kwargs)
+    return wrap
+
+@check_login
 def review(request):
     print("review")
     return render(request, "review_list.html")
 
+@check_login
 def reviewword(request):
     print("reviewword")
 
     return render(request, "review_word.html")
 
+@check_login
 def learn_eng_website_list(request):
     return render(request, "learn_eng_website_list.html")
